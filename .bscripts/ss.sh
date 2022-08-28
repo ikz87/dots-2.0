42211262926d877b743e9c9fe0333074662a1a25
ikz87 <98569017+ikz87@users.noreply.github.com>
2022-08-28 15:29:42 -0600

    Update ss.sh

--- .bscripts/ss.sh
+++ .bscripts/ss.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+mkdir ~/Pictures/Screenshots || true
 date=`date +'%Y-%m-%d_%H:%M:%S'` && scrot ~/Pictures/Screenshots/$date.png -e 'xclip -selection clipboard -target image/png -i $f' && notify-send -u critical -t 3000 -i ~/Pictures/Screenshots/$date.png "Screenshot taken"
