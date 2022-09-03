a6600430e45be45a5bb3a920439bf0b49b3ea36d
ikz87 <iikz87ii@gmail.com>
2022-09-03 03:33:26 -0600

    Changed a glyph

--- .bscripts/rofi.sh
+++ .bscripts/rofi.sh
@@ -18,13 +18,13 @@ case $1 in
         xdotool keydown Tab
     ;;
     outopts)
-        option=`printf "\n\n\n⏻\n" | \
+        option=`printf "\n\n\n⏻\n" | \
         rofi -config $roficonf/logout.rasi -dmenu -p "Select logout" -font "Caskaydia Cove Nerd Font 18"`
         case $option in
             "")ñ
                 bspc wm -r
             ;;
-            "")
+            "")
                 ~/.config/bspwm/quit.sh
             ;;
             "")
