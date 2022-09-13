d55f39a1dbf3a4ca4899586ebf70a03a1df7abe1
ikz87 <iikz87ii@gmail.com>
2022-09-12 18:39:55 -0600

    Add screenshot notification sound trigger

--- .bscripts/notif_sounds.sh
+++ .bscripts/notif_sounds.sh
@@ -3,5 +3,8 @@
 if [[ $2 == "Volume"* ]] 
 then
     play -v 0.8 ~/Documents/Sounds/volume_notif.wav
+elif [[ $2 == *"Screenshot"* ]] || [[ $2 == *"Flameshot"* ]]
+then
+    play -v 0.8 ~/Documents/Sounds/screenshot_notif.wav
 fi;
 
