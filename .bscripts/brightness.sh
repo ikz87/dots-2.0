9550f71b1866d8cc39c59873d3d2d1e8774de867
ikz87 <iikz87ii@gmail.com>
2023-01-15 11:02:01 -0600

    Use light instead of brightlight

--- .bscripts/brightness.sh
+++ .bscripts/brightness.sh
@@ -5,16 +5,14 @@
 # $./brightness.sh down
 
 function get_brightness {
-    var=`brightlight -r -p`
-    echo "${var##* }" | sed 's/[^0-9]*//g'
+    var=`light -G`
+    echo "${var##* }" | sed 's/[^0-9][^.]*//g'
 }
 
 function send_notification {
     DIR=`dirname "$0"`
     brightness=`get_brightness`
     icon_name="${HOME}/Pictures/Important/icons/other/b.png"
-    #bn=$(( (brightness + 5) / 5 ))
-    #bar=$(seq -s "" $bn | sed 's/[0-9]//g')
 
     # Send the notification
     dunstify "Brightness: $brightness%" -h int:value:$brightness -i "$icon_name" -t 1000 --replace=555 -u critical
@@ -28,17 +26,17 @@ case $1 in
 	    inc=$(( 5 - rem ))
         brightness=$(( brightness + inc ))
         [[ $brightness -eq 0 ]] && brightness=1
-	    brightlight -w $brightness -p  > /dev/null
+	    light -S $brightness  > /dev/null
 	    send_notification
 	;;
     down)
-        # Lower the brightness (+ 5%)              
+        # Lower the brightness (- 5%)              
         brightness=`get_brightness`             
         rem=$(( (brightness - 5) % 5 ))         
         inc=$(( 5 + rem ))                                    
         brightness=$(( brightness - inc ))    
         [[ $brightness -eq 0 ]] && brightness=1    
-	    brightlight -w $brightness -p  > /dev/null
+        light -S $brightness > /dev/null
 	    send_notification
 	;;
 esac
