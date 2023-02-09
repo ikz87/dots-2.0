a18fbcf06023cf34c91f8f57ffa5242e89d5ad51
ikz87 <iikz87ii@gmail.com>
2023-02-08 18:49:07 -0600

    Kill needed processes when exiting bspwm

--- Configs/bspwm/quit.sh
+++ Configs/bspwm/quit.sh
@@ -6,6 +6,8 @@ killall sxhkd
 killall dunst
 killall idle.sh
 killall eww
-killall some.sh
-killall dbus-daemon
-killall bspwm
+killall media_info
+killall workspaces
+killall layout
+killall winevents.sh
+bspc quit
