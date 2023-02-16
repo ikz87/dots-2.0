25b047c9a6392bcf2f3bf00569fa03e75ef6f204
ikz87 <iikz87ii@gmail.com>
2023-02-16 15:40:08 -0600

    Major changes (fonts, install fix, eww.scss, etc)

--- Bscripts/wpm_reports.sh
+++ Bscripts/wpm_reports.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-# Switches workspaces an sends a pop up message
-# using pop_report.
-# You can call this script like this:
-# wpm_reports.sh [move|select|jump] [(+1|-1)|(+1|-1)|(0|1|2|3|4)]
-
 # Glyphs
-selected="●"
-wp="○" 
-left=""
-right=""
+selected=""
+wp=""
+left=""
+right=""
 
 
 # Variables
@@ -18,6 +13,8 @@ current=`xdotool get_desktop`
 duration=1000
 font_size=90
 topic="workspace"
+separator_size="+0"
+space_size="+0"
 
 function report_jump {
     for (( i = 0; i < $total; i++)); do
@@ -26,7 +23,7 @@ function report_jump {
         else
             echo -n $wp
         fi;
-        [[ $i < $(( total - 1 )) ]] && echo -n '<font size="-4"> </font>'
+        [[ $i < $(( total - 1 )) ]] && echo -n "<span style=\"color: #00000000\"><font size=\"$space_size\">$right</font></span>"
     done
 }
 
@@ -41,20 +38,19 @@ function report_move {
     fi;
 
     for (( i = 0; i < $total; i++ )); do
-        separator='<font size="-4"> </font>' 
+        separator="<span style=\"color: #00000000\"><font size=\"$space_size\">$main</font></span>"
         if [[ $i -eq $target ]]; then
             echo -n $selected 
-            [[ $main == $left ]] && separator='<font size="-1">'$main'</font>'
+            [[ $main == $left ]] && separator="<font size=\"$separator_size\">$main</font>"
         else
             echo -n $wp
         fi;
-        [[ $main == $right ]] && [[ $((i + 1)) -eq $target ]] && separator='<font size="-1">'$main'</font>'
-        ([[ $((current - target)) -eq $((total - 1)) ]] || [[ $((target - current)) -eq $((total - 1)) ]]) && separator='<font size="-1">'$sec'</font>'
+        [[ $main == $right ]] && [[ $((i + 1)) -eq $target ]] && separator="<font size=\"$separator_size\">$main</font>"
+        ([[ $((current - target)) -eq $((total - 1)) ]] || [[ $((target - current)) -eq $((total - 1)) ]]) && separator="<font size=\"$separator_size\">$sec</font>"
         [[ $i -lt $((total - 1)) ]] && echo -n "$separator"
     done            
 }
 
-# Old hacky pseudo animations
 function bsmove {
 #    # Exit if last move hasn't finished
 #	`cat /tmp/moving` && exit 1
@@ -148,7 +144,7 @@ case $1 in
         pop_report -m "$message" -d $duration -t $topic -o\
             "font-size: $font_size\px"\
             "padding: 0"\
-            "padding-bottom: 15px"\
+            "padding-bottom: 5px"\
             "padding-right: 40px"\
             "padding-left: 40px"&
         bselect
@@ -161,7 +157,7 @@ case $1 in
         pop_report -m "$message" -d $duration -t $topic -o\
             "font-size: $font_size\px"\
             "padding: 0"\
-            "padding-bottom: 15px"\
+            "padding-bottom: 5px"\
             "padding-right: 43px"\
             "padding-left: 40px"&
         bselect
@@ -176,7 +172,7 @@ case $1 in
         pop_report -m "$message" -d $duration -t $topic -o\
             "font-size: $font_size\px"\
             "padding: 0"\
-            "padding-bottom: 15px"\
+            "padding-bottom: 5px"\
             "padding-right: 40px"\
             "padding-left: 40px"&
         bsmove  
--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ create_backup()
     mv ~/.config/wpg $old/Configs/ 2> /dev/null
     mv ~/.config/picom.conf $old/Configs/ 2> /dev/null
     mv ~/.config/rice_assets/ $old/Assets/ 2> /dev/null
-    mv ~/.local/share/fonts/* "$script_dir"/Fonts 2> /dev/null
+    cp ~/.local/share/fonts/* "$script_dir"/Fonts 2> /dev/null
     mv ~/.local/share/fonts/ $old/Fonts 2> /dev/null
     mv ~/.cache/wal/ $old/Walcache/ 2> /dev/null
     echo "done, you can find your backups at $old/"
@@ -77,7 +77,7 @@ done
 ln -s -f -T "$script_dir"/Assets ${HOME}/.config/rice_assets
 ln -s -f -T "$script_dir"/Bscripts ${HOME}/.bscripts
 cp -r -f -T "$script_dir"/Walcache ${HOME}/.cache/wal
-ln -s -f -T"$script_dir"/Fonts ${HOME}/.local/share/fonts
+ln -s -f -T "$script_dir"/Fonts ${HOME}/.local/share/fonts
 echo "done"
 
 echo -n "Changing script permissions... "
