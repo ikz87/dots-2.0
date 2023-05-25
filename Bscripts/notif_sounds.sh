ce60481266c4669a21288e43c1b803f727a4a009
ikz87 <iikz87ii@gmail.com>
2023-05-25 00:15:44 -0600

    Lower notification sound effect volume

--- Bscripts/notif_sounds.sh
+++ Bscripts/notif_sounds.sh
@@ -2,9 +2,9 @@
 
 if [[ $2 == "Volume"* ]] 
 then
-    play -v 0.8 ~/.config/rice_assets/Sounds/volume_notif.wav
+    play -v 0.5 ~/.config/rice_assets/Sounds/volume_notif.wav
 elif [[ $2 == *"Screenshot"* ]] || [[ $2 == *"Flameshot"* ]]
 then
-    play -v 0.8 ~/.config/rice_assets/Sounds/screenshot_notif.wav
+    play -v 0.5 ~/.config/rice_assets/Sounds/screenshot_notif.wav
 fi;
 
